239e80f655fb122b6e496a7cb46f8056b66bca41
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2025-11-09 06:22:07 -0500

    init: sapui5-task2

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sapui5task2.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapui5task2/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
