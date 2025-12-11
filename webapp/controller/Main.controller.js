033155cb1a8937e88010abf25c955cdfaadcb9bf
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2025-12-11 05:54:09 -0500

    feat: Tab Bookmark

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -5,6 +5,20 @@ sap.ui.define(["sapui5task2/controller/BaseController"], (BaseController) => {
       if (BaseController.prototype.onInit) {
         BaseController.prototype.onInit.apply(this, arguments);
       }
+      const oRouter = this.getRouter();
+      oRouter.getRoute("RouteMain").attachPatternMatched(this._onRouteMatched, this);
+      oRouter.getRoute("tab").attachPatternMatched(this._onRouteMatched, this);
+    },
+
+    _onRouteMatched(oEvent) {
+      const oArguments = oEvent.getParameter("arguments");
+      const sTabKey = oArguments.tabKey || "json";
+      this.byId("iconTabBar").setSelectedKey(sTabKey);
+    },
+
+    onTabSelect(oEvent) {
+      const sKey = oEvent.getParameter("key");
+      this.getRouter().navTo("tab", { tabKey: sKey });
     },
   });
 });
