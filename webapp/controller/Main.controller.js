48c0a3ef1c0d431fd927d396cc9a9c2c2bc5616f
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2025-12-12 05:09:39 -0500

    feat: tabs lazy loading

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,18 +7,46 @@ sap.ui.define(["sapui5task2/controller/BaseController"], (BaseController) => {
       }
       const oRouter = this.getRouter();
       oRouter.getRoute("RouteMain").attachPatternMatched(this._onRouteMatched, this);
-      oRouter.getRoute("tab").attachPatternMatched(this._onRouteMatched, this);
+      oRouter.getRoute("jsonTab").attachPatternMatched(this._onRouteMatched, this);
+      oRouter.getRoute("odataV2Tab").attachPatternMatched(this._onRouteMatched, this);
+      oRouter.getRoute("odataV4Tab").attachPatternMatched(this._onRouteMatched, this);
     },
 
     _onRouteMatched(oEvent) {
-      const oArguments = oEvent.getParameter("arguments");
-      const sTabKey = oArguments.tabKey || "json";
+      const sRouteName = oEvent.getParameter("name");
+      let sTabKey;
+      switch (sRouteName) {
+        case "RouteMain":
+          this.getRouter().navTo("jsonTab", {}, true);
+          return;
+        case "jsonTab":
+          sTabKey = "json";
+          break;
+        case "odataV2Tab":
+          sTabKey = "odataV2";
+          break;
+        case "odataV4Tab":
+          sTabKey = "odataV4";
+          break;
+      }
       this.byId("iconTabBar").setSelectedKey(sTabKey);
     },
 
     onTabSelect(oEvent) {
       const sKey = oEvent.getParameter("key");
-      this.getRouter().navTo("tab", { tabKey: sKey });
-    },
+      let sRouteName;
+      switch (sKey) {
+        case "json":
+          sRouteName = "jsonTab";
+          break;
+        case "odataV2":
+          sRouteName = "odataV2Tab";
+          break;
+        case "odataV4":
+          sRouteName = "odataV4Tab";
+          break;
+      }
+      this.getRouter().navTo(sRouteName);
+    }
   });
 });
