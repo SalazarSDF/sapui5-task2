a021119234b14be4771bf1f45a19ef4976a896c1
salas <salas@debian-BULLSEYE-live-builder-AMD64>
2025-12-18 00:10:33 -0500

    feat: [Task 6-2] oData v4 – Record Creation

--- webapp/controller/oDataV4.controller.js
+++ webapp/controller/oDataV4.controller.js
@@ -4,8 +4,9 @@ sap.ui.define(
     "sap/m/MessageBox",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sapui5task2/controller/parts/productV2Types",
   ],
-  (BaseController, MessageBox, MessageToast, JSONModel) => {
+  (BaseController, MessageBox, MessageToast, JSONModel, productTypes) => {
     "use strict";
     return BaseController.extend("sapui5task2.controller.oDataV4", {
       onInit() {
@@ -16,7 +17,13 @@ sap.ui.define(
           selectedRows: [],
         });
         this.setModel(oViewModel, "view");
+
+        Object.keys(productTypes).forEach((type) => {
+          productTypes[type] = productTypes[type].bind(this);
+        });
       },
+
+      productTypes: productTypes,
 
       onProductSelect: function (oEvent) {
         const oTable = oEvent.getSource();
@@ -52,6 +59,174 @@ sap.ui.define(
               this.getI18nText("deleteFailedMessage") + ": " + oError.message,
             );
           });
+      },
+
+      onOpenProductDialog: async function () {
+        this.oProductDialog ??= await this.loadFragment({
+          name: "sapui5task2.fragments.ProductDialogV4",
+        });
+        //        const oListBinding = this.byId("products_table_V4").getBinding("items");
+
+        const oModel = this.getModel("odataV4");
+        const oCreateBinding = oModel.bindList(
+          "/Products",
+          undefined,
+          undefined,
+          undefined,
+          {
+            $$updateGroupId: "createGroup",
+          },
+        );
+        const oInitialProperties = {
+          Name: "",
+          Description: "",
+          ReleaseDate: null,
+          DiscontinuedDate: null,
+          Rating: 2,
+          Price: 0.0,
+        };
+
+        const oNewContext = await oCreateBinding.create(oInitialProperties);
+
+        this.oProductDialog.setBindingContext(oNewContext, "odataV4");
+        const oDialogProduct = new JSONModel({
+          validation: {
+            Name: { errorMessage: "", isTouched: false },
+            Description: { errorMessage: "", isTouched: false },
+            ReleaseDate: { errorMessage: "", isTouched: false },
+            DiscontinuedDate: { errorMessage: "", isTouched: false },
+            Rating: { errorMessage: "", isTouched: false },
+            Price: { errorMessage: "", isTouched: false },
+            isValid: false,
+          },
+        });
+        this.oProductDialog.setModel(oDialogProduct, "dialogProduct");
+        this.oProductDialog.open();
+        this.onValidateForm();
+      },
+      onValidateForm: function (oEvent, bForce) {
+        const oDialogModel = this.oProductDialog.getModel("dialogProduct");
+        const oValidation = oDialogModel.getProperty("/validation");
+        const oContext = this.oProductDialog.getBindingContext("odataV4");
+        const oDialogProduct = oContext.getObject();
+        const aFormFields = [
+          {
+            id: "product_name_input_V4",
+            name: "Name",
+            value: oDialogProduct.Name,
+            validateValue: this.productTypes.validateProductName,
+          },
+          {
+            id: "product_description_input_V4",
+            name: "Description",
+            value: oDialogProduct.Description,
+            validateValue: this.productTypes.validateProductDescription,
+          },
+          {
+            id: "product_release_date_picker_V4",
+            name: "ReleaseDate",
+            value: oDialogProduct.ReleaseDate,
+            validateValue: this.productTypes.validateReleaseDate,
+          },
+          {
+            id: "product_discontinued_date_picker_V4",
+            name: "DiscontinuedDate",
+            value: oDialogProduct.DiscontinuedDate,
+            validateValue: this.productTypes.validateDiscontinuedDate,
+          },
+          {
+            id: "product_rating_input_V4",
+            name: "Rating",
+            value: oDialogProduct.Rating,
+            validateValue: this.productTypes.validateRating,
+          },
+          {
+            id: "product_price_input_V4",
+            name: "Price",
+            value: oDialogProduct.Price,
+            validateValue: this.productTypes.validatePrice,
+          },
+        ].map((formField) => ({
+          ...formField,
+          isTouched: oValidation[formField.name].isTouched,
+        }));
+        if (oEvent) {
+          const sChangedId = oEvent.getSource().getId().split("--").pop();
+          const oChangedField = aFormFields.find(
+            (oField) => oField.id === sChangedId,
+          );
+          if (oChangedField) {
+            oChangedField.isTouched = true;
+            oDialogModel.setProperty(
+              `/validation/${oChangedField.name}/isTouched`,
+              true,
+            );
+          }
+        }
+        if (bForce) {
+          aFormFields.forEach((oField) => {
+            oField.isTouched = true;
+            oDialogModel.setProperty(
+              `/validation/${oField.name}/isTouched`,
+              true,
+            );
+          });
+        }
+        const errors = {};
+        aFormFields.forEach((oField) => {
+          const oControl = this.byId(oField.id);
+          try {
+            oField.validateValue(oField.value);
+            if (oField.isTouched) {
+              oControl.setValueState("None");
+            }
+          } catch (oError) {
+            if (oField.isTouched) {
+              oControl.setValueState("Error");
+            }
+            oDialogModel.setProperty(
+              `/validation/${oField.name}/errorMessage`,
+              oError.message,
+            );
+            errors[oField.name] = oError.message;
+          }
+        });
+        oDialogModel.setProperty(
+          `/validation/isValid`,
+          Object.keys(errors).length === 0,
+        );
+      },
+      onConfirmProduct: function () {
+        this.onValidateForm(null, true);
+        const oDialogModel = this.oProductDialog.getModel("dialogProduct");
+        if (!oDialogModel.getProperty("/validation/isValid")) {
+          MessageBox.error(this.getI18nText("saveFailedMessage"));
+          return;
+        }
+        const oContext = this.oProductDialog.getBindingContext("odataV4");
+        const oModel = this.getModel("odataV4");
+        const oListBinding = this.byId("products_table_V4").getBinding("items")
+
+        oModel
+          .submitBatch("createGroup")
+          .then(() => {
+            MessageToast.show(this.getI18nText("addSuccessMessage"));
+            this.onCloseProductDialog();
+            oListBinding.refresh();
+          })
+          .catch((oError) => {
+            oContext.delete("createGroup");
+            MessageBox.error(
+              this.getI18nText("addFailedMessage") + ": " + oError.message,
+            );
+          });
+      },
+      onCloseProductDialog: function () {
+        const oContext = this.oProductDialog.getBindingContext("odataV4");
+        if (oContext.created() !== undefined) {
+          oContext.delete("createGroup");
+        }
+        this.oProductDialog.close();
       },
     });
   },
